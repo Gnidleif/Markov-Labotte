6e32b535e55c56ed62064c275196481b8b10a5f6
Gnidleif <gnidleif@gmail.com>
2018-11-29 14:43:24 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
 import re
 from random import randint
 
-def run(args):
-    words = "hi my name is Banana and I live in a box that I like very much and I can live there as long as I want"
+def makeChain(words):
     rgx = re.compile(r'[^A-Za-z]')
     keys = [rgx.sub('', key) for key in words.split(' ')]
 
@@ -12,7 +11,15 @@ def run(args):
             chain[keys[i]] = []
         chain[keys[i]].append(keys[i+1])
 
-    print(' '.join([chain[word][randint(0, len(chain[word])-1)] for word in chain]))
+    return chain
+
+def generate(chain, length):
+    return ' '.join([chain[word][randint(0, len(chain[word])-1)] for word in chain])
+
+def run(args):
+    words = "hi my name is Banana and I live in a box that I like very much and I can live there as long as I want"
+    chain = makeChain(words)
+    print(generate(chain, 20))
 
 if __name__ == "__main__":
     import sys
